7892c4432e9f6059e4d784611dbc0bfef561c745
Thomas Weiser <git@thomasweiser.de>
2018-10-09 14:19:10 +0200

    Add offset-based pagination module

---
+++ frontend/src/Pagination/Offset/Page.elm
@@ -0,0 +1,102 @@
+module Pagination.Offset.Page
+    exposing
+        ( Page
+        , entries
+        , isFirstPage
+        , PageResult
+        , initialPageResult
+        , updatePageResultFromResult
+        , loadingPageResult
+        , Position(..)
+        , positionToOffset
+        )
+
+import Graphqelm.Extra
+
+
+type alias Page itemModel =
+    { offset : Int
+    , hasNextPage : Bool
+    , content : List itemModel
+    }
+
+
+type Position
+    = First
+    | Previous
+    | Next
+
+
+entries : Page itemModel -> List itemModel
+entries page =
+    page.content
+
+
+isFirstPage : Page itemModel -> Bool
+isFirstPage page =
+    page.offset == 0
+
+
+type alias PageResult itemModel =
+    { loading : Bool
+    , error : Maybe Graphqelm.Extra.StrippedError
+    , page : Maybe (Page itemModel)
+    }
+
+
+initialPageResult : PageResult itemModel
+initialPageResult =
+    { loading = False
+    , error = Nothing
+    , page = Nothing
+    }
+
+
+updatePageResultFromResult :
+    Result Graphqelm.Extra.StrippedError (Page itemModel)
+    -> PageResult itemModel
+    -> PageResult itemModel
+updatePageResultFromResult result pageResult =
+    case result of
+        Err err ->
+            { pageResult | loading = False, error = Just err }
+
+        Ok content ->
+            { loading = False, error = Nothing, page = Just content }
+
+
+loadingPageResult : PageResult itemModel -> PageResult itemModel
+loadingPageResult pageResult =
+    { pageResult | loading = True }
+
+
+positionToOffset :
+    Int
+    -> Maybe (Page nodeType)
+    -> Position
+    -> Int
+positionToOffset pageSize referencePage position =
+    case position of
+        First ->
+            0
+
+        Previous ->
+            case referencePage of
+                Nothing ->
+                    0
+
+                Just { offset } ->
+                    offset
+                        - pageSize
+                        |> Basics.max 0
+
+        Next ->
+            case referencePage of
+                Nothing ->
+                    0
+
+                Just { offset, hasNextPage } ->
+                    if hasNextPage then
+                        offset + pageSize
+                    else
+                        offset
