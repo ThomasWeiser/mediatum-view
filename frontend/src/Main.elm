4f18f5e1c290f98f3f12f62c8b65f5bed21804e1
Thomas Weiser <git@thomasweiser.de>
2018-11-07 12:25:40 +0100

    Split off module Page from Main

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,28 +1,16 @@
 module Main exposing (main)
 
-import Article
 import Browser
 import Browser.Navigation
-import Url exposing (Url)
-import Cmd.Extra
-import Controls
-import Dict
-import Folder exposing (FolderCounts)
 import Html exposing (Html)
 import Html.Attributes
 import Html.Events
-import Icons
-import Maybe.Extra
-import Query exposing (Query)
-import Tree
+import Page
+import Url exposing (Url)
 
 
 type alias Model =
-    { query : Query
-    , tree : Tree.Model
-    , controls : Controls.Model
-    , folderCounts : FolderCounts
-    , article : Article.Model
+    { page : Page.Model
     }
 
 
@@ -40,36 +28,17 @@ main =
 
 type Msg
     = NoOp
-    | TreeMsg Tree.Msg
-    | ControlsMsg Controls.Msg
-    | ArticleMsg Article.Msg
+    | PageMsg Page.Msg
 
 
 init : () -> Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
 init flags url navigationKey =
     let
-        ( treeModel, treeCmd ) =
-            Tree.init
-
-        controlsModel =
-            Controls.init ()
-
-        ( articleModel, articleCmd ) =
-            Article.initEmpty ()
-
-        model =
-            { query = Query.emptyQuery
-            , tree = treeModel
-            , controls = controlsModel
-            , folderCounts = Dict.empty
-            , article = articleModel
-            }
+        ( pageModel, pageCmd ) =
+            Page.init ()
     in
-    ( model
-    , Cmd.batch
-        [ Cmd.map TreeMsg treeCmd
-        , Cmd.map ArticleMsg articleCmd
-        ]
+    ( { page = pageModel }
+    , Cmd.map PageMsg pageCmd
     )
 
 
@@ -79,143 +48,23 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        TreeMsg subMsg ->
+        PageMsg subMsg ->
             let
-                ( subModel, subCmd, changedSelection ) =
-                    Tree.update subMsg model.tree
-
-                model1 =
-                    { model | tree = subModel }
-
-                maybeSelectedFolder =
-                    Tree.selectedFolder model1.tree
+                ( subModel, subCmd ) =
+                    Page.update subMsg model.page
             in
-            (case ( changedSelection, maybeSelectedFolder ) of
-                ( True, Just selectedFolder ) ->
-                    startQuery
-                        (Query.setFolder
-                            selectedFolder
-                            model1.query
-                        )
-                        model1
-
-                _ ->
-                    ( model1, Cmd.none )
+            ( { model
+                | page = subModel
+              }
+            , Cmd.map PageMsg subCmd
             )
-                |> Cmd.Extra.addCmd (Cmd.map TreeMsg subCmd)
-
-        ControlsMsg subMsg ->
-            let
-                ( subModel, subCmd, subReturn ) =
-                    Controls.update
-                        { query = model.query }
-                        subMsg
-                        model.controls
-
-                model1 =
-                    { model | controls = subModel }
-            in
-            (case subReturn of
-                Controls.NoReturn ->
-                    ( model1, Cmd.none )
-
-                Controls.MapQuery queryMapping ->
-                    startQuery (queryMapping model1.query) model1
-            )
-                |> Cmd.Extra.addCmd (Cmd.map ControlsMsg subCmd)
-
-        ArticleMsg subMsg ->
-            let
-                ( subModel, subCmd, subReturn ) =
-                    Article.update
-                        { query = model.query }
-                        subMsg
-                        model.article
-
-                model1 =
-                    { model | article = subModel }
-            in
-            (case subReturn of
-                Article.NoReturn ->
-                    ( model1, Cmd.none )
-
-                Article.FolderCounts folderCounts1 ->
-                    ( { model1 | folderCounts = folderCounts1 }
-                    , Cmd.none
-                    )
-
-                Article.MapQuery queryMapping ->
-                    startQuery (queryMapping model1.query) model1
-            )
-                |> Cmd.Extra.addCmd (Cmd.map ArticleMsg subCmd)
-
-
-startQuery : Query -> Model -> ( Model, Cmd Msg )
-startQuery query model =
-    let
-        ( articleModel, articleCmd ) =
-            Article.initWithQuery query
-    in
-    ( { model
-        | query = Debug.log "startQuery" query
-        , article = articleModel
-        , folderCounts = Dict.empty
-      }
-    , Cmd.map ArticleMsg articleCmd
-    )
-
-
-andThenUpdate : Msg -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
-andThenUpdate msg ( model1, cmd1 ) =
-    let
-        ( model2, cmd2 ) =
-            update msg model1
-    in
-    ( model2, Cmd.batch [ cmd1, cmd2 ] )
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "mediaTUM View"
-    , body = [ viewPage model ]
-    }
-
-
-viewPage : Model -> Html Msg
-viewPage model =
-    Html.div [ Html.Attributes.class "page-container" ]
-        [ Icons.definitions
-        , Html.header []
-            [ Html.h2 []
-                [ Html.div []
-                    [ Html.span
-                        [ Html.Attributes.class "title" ]
-                        [ Html.text "mediaTUM view" ]
-                    , Html.span
-                        [ Html.Attributes.class "subtitle"
-                        , Html.Attributes.title "You may click here to start an example query."
-                        , Html.Events.onClick (ControlsMsg Controls.submitExampleQuery)
-                        ]
-                        [ Html.text "WIP 2018-10-29" ]
-                    , Html.img
-                        [ Html.Attributes.alt "TUM Logo"
-                        , Html.Attributes.src "logo_tum.png"
-                        , Html.Attributes.style "float" "right"
-                        ]
-                        []
-                    ]
-                ]
-            , Controls.view { query = model.query } model.controls
-                |> Html.map ControlsMsg
-            ]
-        , Html.main_ []
-            [ Html.aside []
-                [ Html.map TreeMsg <| Tree.view model.tree model.folderCounts
-                ]
-            , Html.map ArticleMsg <|
-                Article.view
-                    model.tree
-                    { query = model.query }
-                    model.article
-            ]
+    , body =
+        [ Page.view model.page
+            |> Html.map PageMsg
         ]
+    }
