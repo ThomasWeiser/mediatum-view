cabb229fe9bc0f745e1cfe58031472b83cdc220f
Thomas Weiser <git@thomasweiser.de>
2019-04-29 11:21:13 +0200

    Document Api functions and illustrate them using GraphQL notation

--- frontend/src/Api.elm
+++ frontend/src/Api.elm
@@ -1,18 +1,33 @@
 module Api exposing
-    ( ApiError
-    , Response
-    , makeMutationRequest
-    , makeQueryRequest
-    , queryDocumentDetails
-    , queryFolderDocumentsPage
-    , queryFolderFolderCounts
-    , queryFtsFolderCounts
-    , queryFtsPage
-    , queryGenericNode
-    , querySubfolder
-    , queryToplevelFolder
+    ( Response, ApiError
+    , makeQueryRequest, makeMutationRequest
+    , queryToplevelFolder, querySubfolder, queryGenericNode, queryFolderDocumentsPage, queryFolderFolderCounts, queryFtsPage, queryFtsFolderCounts, queryDocumentDetails
     , updateDocumentAttribute
     )
+
+{-| Definitions of all specific GraphQL requests needed for the application.
+
+
+# Types
+
+@docs Response, ApiError
+
+
+# Run GraphQL Requests
+
+@docs makeQueryRequest, makeMutationRequest
+
+
+# GraphQL Query Definitions
+
+@docs queryToplevelFolder, querySubfolder, queryGenericNode, queryFolderDocumentsPage, queryFolderFolderCounts, queryFtsPage, queryFtsFolderCounts, queryDocumentDetails
+
+
+# GraphQL Mutation Definitions
+
+@docs updateDocumentAttribute
+
+-}
 
 import Dict
 import Document exposing (Document, DocumentId)
@@ -53,24 +68,45 @@ import Query exposing (Query)
 import Query.Attribute
 
 
+{-| Endpoint for backend's GraphQL service.
+-}
 apiUrl : String
 apiUrl =
     "/graphql"
 
 
+{-| Number of results per page used for pagination.
+Currently hard-coded.
+-}
 pageSize : Int
 pageSize =
     10
 
 
+{-| A query specific Result type.
+-}
 type alias Response decodesTo =
     Result ApiError decodesTo
 
 
+{-| Represents an error from running a GraphQL request.
+-}
 type alias ApiError =
     Graphql.Extra.StrippedError
 
 
+{-| Create a GraphQL query.
+
+Takes a tagger function for wrapping the result,
+which is either a query specific type representing the queried data
+or an ApiError.
+
+The query itself is given as a `Graphql.SelectionSet.SelectionSet`
+, see [elm-graphql](https://package.elm-lang.org/packages/dillonkearns/elm-graphql/latest/Graphql-SelectionSet).
+There are functions in this module to produce these selection sets for all
+relevant queries of the application.
+
+-}
 makeQueryRequest :
     (Response decodesTo -> msg)
     -> SelectionSet decodesTo Graphql.Operation.RootQuery
@@ -82,6 +118,11 @@ makeQueryRequest tagger selectionSet =
             (Result.mapError Graphql.Extra.stripError >> tagger)
 
 
+{-| Create a GraphQL mutation.
+
+Like `makeQueryRequest` but for mutations.
+
+-}
 makeMutationRequest :
     (Response decodesTo -> msg)
     -> SelectionSet decodesTo Graphql.Operation.RootMutation
@@ -93,6 +134,24 @@ makeMutationRequest tagger selectionSet =
             (Result.mapError Graphql.Extra.stripError >> tagger)
 
 
+{-| Get the root folders and their sub-folders.
+
+_GraphQL notation:_
+
+    query {
+        allFolders(isRoot: true) {
+            nodes {
+                ...folderData
+                subfolders {
+                    nodes {
+                        ...folderData
+                    }
+                }
+            }
+        }
+    }
+
+-}
 queryToplevelFolder : SelectionSet (List ( Folder, List Folder )) Graphql.Operation.RootQuery
 queryToplevelFolder =
     Graphql.Query.allFolders
@@ -104,6 +163,19 @@ queryToplevelFolder =
         (Graphql.Object.FoldersConnection.nodes folderNodeWithSubfolders)
 
 
+{-| Get the sub-folders of a list of folders.
+
+_GraphQL notation:_
+
+    query {
+        allFolders(parentIds: $listOfFolderIds) {
+            nodes {
+                ...folderData
+            }
+        }
+    }
+
+-}
 querySubfolder : List FolderId -> SelectionSet (List Folder) Graphql.Operation.RootQuery
 querySubfolder folderIds =
     Graphql.Query.allFolders
@@ -115,6 +187,22 @@ querySubfolder folderIds =
         (Graphql.Object.FoldersConnection.nodes folderNode)
 
 
+{-| Get a folder or a document with a given mediaTUM id.
+
+_GraphQL notation:_
+
+    query {
+        genericNodeById(id: $mediatumNodeId) {
+            asFolder {
+                ...folderLineage
+            }
+            asDocument {
+                ...documentNode
+            }
+        }
+    }
+
+-}
 queryGenericNode : Int -> SelectionSet GenericNode Graphql.Operation.RootQuery
 queryGenericNode nodeId =
     let
@@ -143,6 +231,19 @@ queryGenericNode nodeId =
         |> SelectionSet.nonNullOrFail
 
 
+{-| Selection set on a Folder to get basic properties of the folder.
+
+_GraphQL notation:_
+
+    fragment folderData on Folder {
+        id
+        parentId
+        name
+        isCollection
+        numSubfolder
+    }
+
+-}
 folderNode : SelectionSet Folder Graphql.Object.Folder
 folderNode =
     SelectionSet.succeed Folder.init
@@ -153,6 +254,20 @@ folderNode =
         |> SelectionSet.with (Graphql.Object.Folder.numSubfolder |> SelectionSet.nonNullOrFail)
 
 
+{-| Selection set on a folder to get the basic properties of that folder and of its sub-folders.
+
+_GraphQL notation:_
+
+    fragment folderNodeWithSubfolders on Folder {
+        ...folderNode
+        subfolders {
+            nodes {
+                ...folderNode
+            }
+        }
+    }
+
+-}
 folderNodeWithSubfolders : SelectionSet ( Folder, List Folder ) Graphql.Object.Folder
 folderNodeWithSubfolders =
     SelectionSet.succeed Tuple.pair
@@ -165,6 +280,18 @@ folderNodeWithSubfolders =
             )
 
 
+{-| Selection set on a folder to get the lineage of that folder,
+i.e. a non-emtpy list of folders representing the path from the given folder up to a root folder of the hierarchy.
+
+_GraphQL notation:_
+
+    fragment folderLineage on Folder {
+        lineage {
+            ...folderData
+        }
+    }
+
+-}
 folderLineage : SelectionSet (Nonempty Folder) Graphql.Object.Folder
 folderLineage =
     Graphql.Object.Folder.lineage
@@ -177,6 +304,23 @@ folderLineage =
             )
 
 
+{-| Get all documents of a folder with offet-based pagination
+A list of filters may be used to restrict the documents to be returned.
+
+_GraphQL notation:_
+
+    query {
+        allDocumentsPage(
+            folderId: $folderId
+            attributeTests: $listOfAttributeTestsForFiltering
+            limit: $limitNumberUsedForPagination
+            offset: $offsetNumberUsedForPagination
+        ) {
+            ...documentResultPage
+        }
+    }
+
+-}
 queryFolderDocumentsPage :
     Maybe (Pagination.Offset.Page.Page DocumentResult)
     -> Pagination.Offset.Page.Position
@@ -205,6 +349,21 @@ queryFolderDocumentsPage referencePage paginationPosition folderQuery =
         |> SelectionSet.nonNullOrFail
 
 
+{-| Get the counts of documents within a folder and its sub-folders.
+A list of filters may be used to restrict the documents to be counted.
+
+_GraphQL notation:_
+
+    query {
+        allDocumentsDocset(
+            folderId: $folderId
+            attributeTests: $listOfAttributeTestsForFiltering
+        ) {
+            ...folderAndSubfolderCounts
+        }
+    }
+
+-}
 queryFolderFolderCounts :
     Query.FolderQuery
     -> SelectionSet FolderCounts Graphql.Operation.RootQuery
@@ -224,6 +383,26 @@ queryFolderFolderCounts folderQuery =
         |> SelectionSet.nonNullOrFail
 
 
+{-| Get documents using a full-text search with offet-based pagination.
+A list of filters may be used to restrict the documents to be found.
+
+_GraphQL notation:_
+
+    query {
+        ftsDocumentsPage(
+            folderId: $folderId
+            text: $searchTerm
+            domain: $attrs_or_fulltext
+            language: $english_or_german
+            attributeTests: $listOfAttributeTestsForFiltering
+            limit: $limitNumberUsedForPagination
+            offset: $offsetNumberUsedForPagination
+        ) {
+            ...documentResultPage
+        }
+    }
+
+-}
 queryFtsPage :
     Maybe (Pagination.Offset.Page.Page DocumentResult)
     -> Pagination.Offset.Page.Position
@@ -255,6 +434,25 @@ queryFtsPage referencePage paginationPosition ftsQuery =
         |> SelectionSet.nonNullOrFail
 
 
+{-| Get the counts of documents found by a full-text search.
+The counts are computed for the given folder and each of its sub-folders.
+A list of filters may be used to restrict the documents to be counted.
+
+_GraphQL notation:_
+
+    query {
+        ftsDocumentsDocset(
+            folderId: $folderId
+            text: $searchTerm
+            domain: $attrs_or_fulltext
+            language: $english_or_german
+            attributeTests: $listOfAttributeTestsForFiltering
+        ) {
+            ...folderAndSubfolderCounts
+        }
+    }
+
+-}
 queryFtsFolderCounts :
     Query.FtsQuery
     -> SelectionSet FolderCounts Graphql.Operation.RootQuery
@@ -277,6 +475,22 @@ queryFtsFolderCounts ftsQuery =
         |> SelectionSet.nonNullOrFail
 
 
+{-| Selection set on a Docset to get the counts of documents within.
+
+_GraphQL notation:_
+
+    fragment folderAndSubfolderCounts on Docset {
+        folderCount {
+            ...folderCount
+        }
+        subfolderCounts {
+            nodes {
+                ...folderCount
+            }
+        }
+    }
+
+-}
 folderAndSubfolderCounts : SelectionSet Folder.FolderCounts Graphql.Object.Docset
 folderAndSubfolderCounts =
     SelectionSet.succeed
@@ -298,6 +512,16 @@ folderAndSubfolderCounts =
             )
 
 
+{-| Selection set on a FolderCount to get the count of the selected documents within the folder.
+
+_GraphQL notation:_
+
+    fragment folderCount on FolderCount {
+        count
+        folderId
+    }
+
+-}
 folderCount : SelectionSet ( FolderId, Int ) Graphql.Object.FolderCount
 folderCount =
     SelectionSet.succeed Tuple.pair
@@ -312,6 +536,30 @@ folderCount =
             )
 
 
+{-| Get a page of documents found by searching on an author's name.
+
+The result is paginated according to the Relay specification.
+Up to now this query function is only a preliminary draft.
+
+_GraphQL notation:_
+
+    query {
+        authorSearch(
+            text: $searchTerm
+            first: $optionalRelayPaginationArgument
+            last: $optionalRelayPaginationArgument
+            before: $optionalRelayPaginationArgument
+            after: $optionalRelayPaginationArgument
+        ) {
+            edges {
+                node {
+                    ...documentNode
+                }
+            }
+        }
+    }
+
+-}
 queryAuthorSearch :
     Maybe (Pagination.Relay.Page.Page Document)
     -> Pagination.Relay.Pagination.Position
@@ -336,6 +584,18 @@ queryAuthorSearch referencePage paginationPosition folderId searchString =
         )
 
 
+{-| Get the basic properties of a document selected by its mediaTUM id
+together with the document's attributes selected by the mediaTUM mask "nodebig".
+
+_GraphQL notation:_
+
+    query {
+        documentById(id: $idOfTheDocument) {
+            ...documentNode
+        }
+    }
+
+-}
 queryDocumentDetails :
     DocumentId
     -> SelectionSet (Maybe Document) Graphql.Operation.RootQuery
@@ -349,6 +609,24 @@ queryDocumentDetails documentId =
         (documentNode "nodebig")
 
 
+{-| Set an attribute of a document selected by a mediaTUM id.
+Returns the new document details based on the mediaTUM mask "nodebig".
+
+_GraphQL notation:_
+
+    mutation {
+        updateDocumentAttribute(
+            input: {
+                id: $idOfTheDocument
+                key: $keyOfTheAttribute
+                value: $desiredValueOfTheAttribute
+            }
+        ) {
+            ...documentNode
+        }
+    }
+
+-}
 updateDocumentAttribute :
     DocumentId
     -> String
@@ -373,6 +651,23 @@ updateDocumentAttribute documentId key value =
         )
 
 
+{-| Selection set on a DocumentResultPage to get a page of a paginated list of documents.
+
+The page contains a list of document results as well as some pagination-specific data.
+
+The nested documents are rendered according to a named mediaTUM mask.
+
+_GraphQL notation:_
+
+    fragment documentResultPage on DocumentResultPage {
+        offset
+        hasNextPage
+        content {
+            ...documentResult
+        }
+    }
+
+-}
 documentResultPage :
     String
     -> SelectionSet (Pagination.Offset.Page.Page DocumentResult) Graphql.Object.DocumentResultPage
@@ -394,6 +689,20 @@ documentResultPage maskName =
             )
 
 
+{-| Selection set on a DocumentResult to get the document data and additional search result characterization
+(position of the result, ranking of the result).
+
+The nested document is rendered according to a named mediaTUM mask.
+
+_GraphQL notation:_
+
+    fragment documentResult on DocumentResult {
+        number
+        distance
+        ...documentNode
+    }
+
+-}
 documentResult : String -> SelectionSet DocumentResult Graphql.Object.DocumentResult
 documentResult maskName =
     SelectionSet.succeed DocumentResult.init
@@ -412,6 +721,21 @@ documentResult maskName =
             )
 
 
+{-| Selection set on a Document to get the basic properties of the document
+together with the document's attributes selected by a named mediaTUM mask.
+
+_GraphQL notation:_
+
+    fragment documentNode on Document {
+        id
+        metadatatype {
+            longname
+        }
+        name
+        valuesByMask(maskName: maskName)
+    }
+
+-}
 documentNode : String -> SelectionSet Document Graphql.Object.Document
 documentNode maskName =
     SelectionSet.succeed Document.init
@@ -444,6 +768,8 @@ documentNode maskName =
             )
 
 
+{-| Decode a JSON string returned from a query that denotes the mata-values of a document.
+-}
 mapJsonToAttributes : Maybe Graphql.Scalar.Json -> List Document.Attribute
 mapJsonToAttributes maybeJson =
     case maybeJson of
@@ -468,6 +794,12 @@ decoderAttributeList =
         ]
 
 
+{-| Configuration object for abstracting Relay pagination functions referring to a list of documents.
+
+It contains all the selection set functions needed for building
+queries according to the Relay pagination specification.
+
+-}
 graphqlDocumentObjects : Connection.GraphqlObjects {} Graphql.Object.DocumentsConnection Graphql.Object.DocumentsEdge Graphql.Object.Document Graphql.Object.PageInfo Graphql.Scalar.Cursor Document
 graphqlDocumentObjects =
     { totalCount = Graphql.Object.DocumentsConnection.totalCount
