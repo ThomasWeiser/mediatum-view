60c05478ca88047baa0fe3fa1edc30f3484df99f
Thomas Weiser <git@thomasweiser.de>
2019-07-19 16:34:12 +0200

    Create new FTS table using bilingual dict

--- backend/src/sql/indexes.sql
+++ backend/src/sql/indexes.sql
@@ -1,37 +1,78 @@
 
--- Define additional indexes on mediatum tables
+-- Preprocess data for FTS and create appropriate indexes
+
+drop schema if exists preprocess cascade;
+create schema if not exists preprocess;
 
 
--- Map searchtypes (aka as domains) to ranking weight classes.
--- This function is used to build the functional indexes (aka. indexes on expressions).
--- The same function has to used in the queries in order to bring the index into being used.
+drop table if exists preprocess.ufts;
 
-create or replace function set_tsvec_searchtype_weight (tsvec tsvector, searchtype text)
-    returns tsvector as $$
-    declare result tsvector;
+create table preprocess.ufts (
+	nid serial not null primary key references mediatum.node(id) on delete cascade,
+	"year" int4 null,
+	tsvec tsvector null
+);
+
+create or replace function preprocess.year_from_attrs (attrs jsonb)
+    returns int4 as $$
     begin
-        case searchtype
-        	when 'attrs' then
-	            result := setweight(tsvec, 'A');
-            when 'fulltext' then
-	            result := setweight(tsvec, 'D');
-            else
-	            result := tsvec;
-        end case;
-        return result;
+        return
+            -- Try to extract a 4 digits year substring from different possible date formats used in attribute field "year"
+            substring((attrs #>> '{year}') from '\d{4}')::int4;
+        
+        exception when others then
+                raise notice 'invalid year: "%".  returning null.', (attrs #>> '{year}');
+                return null;
+    end;
+$$ language plpgsql immutable;
+
+create or replace function preprocess.unified_tsvec_from_attrs_and_fulltext (attrs jsonb, fulltext varchar)
+    returns tsvector as $$
+    declare
+        exc_text text;
+        exc_detail text;
+        exc_hint text;
+    begin
+	    return
+			setweight(to_tsvector('english_german', attrs), 'A')
+			||
+			setweight(to_tsvector(
+                'english_german'
+                -- Limit the fulltext length to avoid "string is too long for tsvector (... bytes, max 1048575 bytes)"
+                , left(fulltext, 1048000)
+                ), 'D');
+
+        exception
+            when others then
+                get stacked diagnostics exc_text = message_text,
+                                        exc_detail = pg_exception_detail,
+                                        exc_hint = pg_exception_hint;
+                raise notice 'Exception:  %\n%\nHint: %', exc_text, exc_detail, exc_hint;
+
+                return null;
     end;
 $$ language plpgsql immutable;
 
 
-create index if not exists fts_english_rum_tsvector_ops
-    on mediatum.fts
- using rum (set_tsvec_searchtype_weight(tsvec, searchtype) rum_tsvector_ops)
- where config = 'english'::text;
+create or replace view preprocess.ufts_as_view as
+    select
+    node.id as id,
+    preprocess.year_from_attrs(node.attrs) as "year",
+    preprocess.unified_tsvec_from_attrs_and_fulltext(node.attrs, node.fulltext) as tsvec
+    from mediatum.node;
 
-create index if not exists fts_german_rum_tsvector_ops
-    on mediatum.fts
- using rum (set_tsvec_searchtype_weight(tsvec, searchtype) rum_tsvector_ops)
- where config = 'german'::text;
+
+insert into preprocess.ufts (nid, "year", tsvec)
+  select * 
+  from preprocess.ufts_as_view
+  where "year" is not null or tsvec is not null
+  -- limit 2000 -- For testing the code just process a tiny fraction of the data
+;
+
+create index if not exists ufts_rum_tsvector_ops
+    on preprocess.ufts
+ using rum (tsvec rum_tsvector_ops);
+
 
 ------------------------------------------------------------------
 
