280f5d248142e9891c63b302b58ed4f60b43adcf
Thomas Weiser <git@thomasweiser.de>
2019-08-11 16:16:00 +0200

    Get needed data in cache
    All needs for requesting API data are represented as a value of the type Need

--- frontend/src/elm/Data/Cache.elm
+++ frontend/src/elm/Data/Cache.elm
@@ -1,10 +1,11 @@
-module Data.Cache exposing (ApiData, Cache, Need(..), Needs)
+module Data.Cache exposing (ApiData, Model, Need(..), Needs)
 
 import Api
 import Api.Queries
 import Basics.Extra
 import Data.Types exposing (..)
 import Dict exposing (Dict)
+import Dict.Extra
 import List.Extra
 import RemoteData exposing (RemoteData(..))
 
@@ -13,10 +14,10 @@ type alias ApiData a =
     RemoteData Api.Error a
 
 
-type alias Cache =
+type alias Model =
     { rootFolderIds : ApiData (List FolderId)
     , folders : Dict FolderId (ApiData Folder)
-    , subFolders : Dict FolderId (ApiData (List FolderId))
+    , subfolderIds : Dict FolderId (ApiData (List FolderId))
     , nodeTypes : Dict Int (ApiData NodeType)
     , folderCounts : Dict Selection (ApiData (Dict FolderId Int))
     , docListPages : Dict Selection (Dict Window (ApiData DocumentsPage))
@@ -32,50 +33,152 @@ type alias Needs =
 
 type Need
     = NeedRootFolderIds
-    | NeedFolder FolderId
+    | NeedSubfolders (List FolderId)
 
 
 
-{-
-   | NeedSubfolders FolderId
-   | NeedNodeType Int
-   | NeedFolderCounts Selection
-   | NeedDocListPage Selection Window
-   | NeedDocument DocumentId
--}
+-- TODO: | NeedNodeType Int
+-- TODO: | NeedFolderCounts Selection
+-- TODO: | NeedDocListPage Selection Window
+-- TODO: | NeedDocument DocumentId
 
 
 type Msg
-    = NoOp
-    | ApiResponseToplevelFolder (Api.Response (List ( Folder, List Folder )))
+    = ApiResponseToplevelFolder (Api.Response (List ( Folder, List Folder )))
+    | ApiResponseSubfolder (List FolderId) (Api.Response (List Folder))
 
 
-requestNeeds : Needs -> Cache -> ( Cache, Cmd Msg )
-requestNeeds needs cache =
+requestNeeds : Needs -> Model -> ( Model, Cmd Msg )
+requestNeeds needs model =
     List.Extra.mapAccuml
         (Basics.Extra.flip requestNeed)
-        cache
+        model
         needs
         |> Tuple.mapSecond Cmd.batch
 
 
-requestNeed : Need -> Cache -> ( Cache, Cmd Msg )
-requestNeed need cache =
+requestNeed : Need -> Model -> ( Model, Cmd Msg )
+requestNeed need model =
     case need of
         NeedRootFolderIds ->
-            case cache.rootFolderIds of
+            case model.rootFolderIds of
                 NotAsked ->
-                    ( { cache
+                    ( { model
                         | rootFolderIds = Loading
                       }
-                      -- todo
                     , Api.sendQueryRequest
                         ApiResponseToplevelFolder
                         Api.Queries.toplevelFolder
                     )
 
                 _ ->
-                    ( cache, Cmd.none )
+                    ( model, Cmd.none )
 
-        NeedFolder folderId ->
-            ( cache, Cmd.none )
+        NeedSubfolders parentIds ->
+            let
+                parentIdsWithUnknownChildren =
+                    List.filter
+                        (\parentId ->
+                            (Dict.get parentId model.subfolderIds |> Maybe.withDefault NotAsked)
+                                == NotAsked
+                        )
+                        parentIds
+            in
+            if List.isEmpty parentIdsWithUnknownChildren then
+                ( model, Cmd.none )
+
+            else
+                ( { model
+                    | subfolderIds =
+                        List.foldl
+                            (\parentId subfolderIds ->
+                                Dict.insert parentId Loading subfolderIds
+                            )
+                            model.subfolderIds
+                            parentIdsWithUnknownChildren
+                  }
+                , Api.sendQueryRequest
+                    (ApiResponseSubfolder parentIds)
+                    (Api.Queries.subfolder parentIdsWithUnknownChildren)
+                )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ApiResponseToplevelFolder (Ok listOfRootFoldersWithSubfolders) ->
+            ( let
+                allNewFolders =
+                    listOfRootFoldersWithSubfolders
+                        |> List.map (Basics.Extra.uncurry (::))
+                        |> List.concat
+              in
+              { model
+                | rootFolderIds =
+                    Success
+                        (List.map (Tuple.first >> .id) listOfRootFoldersWithSubfolders)
+              }
+                |> insertAsFolders allNewFolders
+                |> insertAsSubfolderIds allNewFolders
+            , Cmd.none
+            )
+
+        ApiResponseToplevelFolder (Err error) ->
+            ( { model
+                | rootFolderIds = Failure error
+              }
+            , Cmd.none
+            )
+
+        ApiResponseSubfolder _ (Ok listOfSubfolders) ->
+            ( model
+                |> insertAsFolders listOfSubfolders
+                |> insertAsSubfolderIds listOfSubfolders
+            , Cmd.none
+            )
+
+        ApiResponseSubfolder parentIds (Err error) ->
+            ( { model
+                | subfolderIds =
+                    List.foldl
+                        (\parentId subfolderIds ->
+                            Dict.insert parentId (Failure error) subfolderIds
+                        )
+                        model.subfolderIds
+                        parentIds
+              }
+            , Cmd.none
+            )
+
+
+insertAsFolders : List Folder -> Model -> Model
+insertAsFolders listOfNewFolders model =
+    { model
+        | folders =
+            List.foldl
+                (\folder ->
+                    Dict.insert folder.id (Success folder)
+                )
+                model.folders
+                listOfNewFolders
+    }
+
+
+insertAsSubfolderIds : List Folder -> Model -> Model
+insertAsSubfolderIds allSubfoldersOfSomeNewParents model =
+    { model
+        | subfolderIds =
+            Dict.Extra.filterGroupBy
+                .parent
+                allSubfoldersOfSomeNewParents
+                |> Dict.toList
+                |> List.foldl
+                    (\( parentId, subfolders ) ->
+                        Dict.insert
+                            parentId
+                            (Success
+                                (List.map .id subfolders)
+                            )
+                    )
+                    model.subfolderIds
+    }
