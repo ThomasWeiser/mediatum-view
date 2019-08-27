7be673c2392782e0b3a5da3e86ce17568a948bc5
Thomas Weiser <git@thomasweiser.de>
2019-08-27 13:55:19 +0200

    Display progress or error messages in Article.generic

--- frontend/src/elm/Article/Generic.elm
+++ frontend/src/elm/Article/Generic.elm
@@ -7,8 +7,11 @@ module Article.Generic exposing
     )
 
 import Data.Cache as Cache exposing (ApiData)
-import Data.Types exposing (NodeId)
+import Data.Types exposing (NodeId, NodeType(..))
 import Html exposing (Html)
+import Icons
+import RemoteData exposing (RemoteData(..))
+import Utils
 
 
 type alias Context =
@@ -39,7 +42,57 @@ update msg model =
 
 view : Context -> Model -> Html Msg
 view context model =
+    let
+        remoteDataMessage =
+            case context.nodeIds of
+                Nothing ->
+                    Cache.getRootFolder_DD context.cache
+                        |> RemoteData.map (always "Going to show the root folder")
+
+                Just ( nodeId, Nothing ) ->
+                    Cache.getNodeType_AD context.cache nodeId
+                        |> Cache.asDerivedData
+                        |> Utils.remoteDataCheck
+                            (\nodeType ->
+                                Maybe.map Cache.CacheDataError <|
+                                    if nodeType == NodeIsNeither then
+                                        Just "Node type is neither a folder nor a document"
+
+                                    else
+                                        Nothing
+                            )
+                        |> RemoteData.map (always "Going to show the folder or document")
+
+                Just ( nodeIdOne, Just nodeIdTwo ) ->
+                    RemoteData.map2 Tuple.pair
+                        (Cache.getNodeType_AD context.cache nodeIdOne |> Cache.asDerivedData)
+                        (Cache.getNodeType_AD context.cache nodeIdTwo |> Cache.asDerivedData)
+                        |> Utils.remoteDataCheck
+                            (\( nodeTypeOne, nodeTypeTwo ) ->
+                                Maybe.map Cache.CacheDataError <|
+                                    case ( nodeTypeOne, nodeTypeTwo ) of
+                                        ( NodeIsFolder _, NodeIsDocument ) ->
+                                            Nothing
+
+                                        ( NodeIsFolder _, _ ) ->
+                                            Just "Node is not a document"
+
+                                        ( _, _ ) ->
+                                            Just "Node is not a folder"
+                            )
+                        |> RemoteData.map (always "Going to show document in folder")
+    in
     Html.div []
-        [ Html.text "Article.Generic: "
-        , Html.text <| Debug.toString context.nodeIds
+        [ case remoteDataMessage of
+            NotAsked ->
+                Icons.spinner
+
+            Loading ->
+                Icons.spinner
+
+            Success notification ->
+                Html.text notification
+
+            Failure cacheError ->
+                Html.text (Cache.errorToString cacheError)
         ]
