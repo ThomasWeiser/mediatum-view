a7ede1b3c972b6f329910b265c0788cba23f6c10
Thomas Weiser <git@thomasweiser.de>
2020-09-29 12:04:38 +0200

    Second sort criterion in FTS is nodeid descending

--- backend/src/sql/api-fts.sql
+++ backend/src/sql/api-fts.sql
@@ -50,8 +50,10 @@ create or replace function aux.fts_documents_limited
            where ufts.tsvec @@ fts_query
            
            order by
-                case when sorting = 'by_date' then ufts.year <=| 2147483647 end,
-                case when sorting = 'by_rank' then ufts.tsvec <=> fts_query end
+                ( case when sorting = 'by_date' then ufts.year <=| 2147483647 end
+                , case when sorting = 'by_rank' then ufts.tsvec <=> fts_query end
+                )
+                , ufts.nid desc
            
          ) as fts
     join entity.document on document.id = fts.id
@@ -74,8 +76,10 @@ create or replace function aux.fts_documents_limited
 
     -- For now we sort here once again.
     order by
-        case when sorting = 'by_date' then fts.year <=| 2147483647 end,
-        case when sorting = 'by_rank' then fts.distance end
+        ( case when sorting = 'by_date' then fts.year <=| 2147483647 end
+        , case when sorting = 'by_rank' then fts.distance end
+        )
+        , fts.id desc
     
     limit "limit"
     ;
