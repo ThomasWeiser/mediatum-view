d80f92c9d48d9d9488f2a1e25fa2317f3698910e
Thomas Weiser <git@thomasweiser.de>
2021-02-18 11:13:46 +0100

    Add config parameter where necessary in elm-test code

--- frontend/tests/Tests/Types/Route/Url.elm
+++ frontend/tests/Tests/Types/Route/Url.elm
@@ -5,6 +5,7 @@ import Test exposing (..)
 import TestUtils exposing (..)
 import Tests.Types.Route
 import Types.Aspect as Aspect exposing (Aspect)
+import Types.Config as Config exposing (Config)
 import Types.FilterList as FilterList exposing (FilterList)
 import Types.Id as Id
 import Types.Route as Route
@@ -17,6 +18,17 @@ import Utils
 
 
 -- TODO: Add tests and expectations about ftsFilters and facetFilters
+
+
+testConfig : Config
+testConfig =
+    { serverConfigAdopted = True
+    , defaultPageSize = 10
+    , defaultSorting = ByRank
+    , numberOfFacetValues = 20
+    , ftsAspects = []
+    , facetAspects = []
+    }
 
 
 suite : Test
@@ -51,95 +63,95 @@ suite =
         , describe "URL parsing and building"
             [ testString "https://example.com/abc" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> nothing
             , testString "https://example.com/" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal Route.NoId
                         , .parameters >> .globalFts >> nothing
-                        , Types.Route.Url.toString >> Expect.equal "/"
+                        , Types.Route.Url.toString testConfig >> Expect.equal "/"
                         ]
             , testString "https://example.com/123" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal (Route.OneId (Id.fromInt 123))
                         , .parameters >> .globalFts >> nothing
-                        , .parameters >> .sorting >> Expect.equal Route.defaultSorting
+                        , .parameters >> .sorting >> Expect.equal testConfig.defaultSorting
                         , .parameters >> .ftsFilters >> FilterList.isEmpty >> Expect.true "Expecting emtpy set of ftsFilters"
                         , .parameters >> .facetFilters >> FilterList.isEmpty >> Expect.true "Expecting emtpy set of facetFilters"
-                        , Types.Route.Url.toString >> Expect.equal "/123"
+                        , Types.Route.Url.toString testConfig >> Expect.equal "/123"
                         ]
             , testString "https://example.com/?search=foo" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal Route.NoId
                         , .parameters >> .globalFts >> expectJustSearchTerm "foo"
-                        , Types.Route.Url.toString >> Expect.equal "/?search=foo"
+                        , Types.Route.Url.toString testConfig >> Expect.equal "/?search=foo"
                         ]
             , testString "https://example.com/?sort-by=rank" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal Route.NoId
                         , .parameters >> .globalFts >> nothing
                         , .parameters >> .sorting >> Expect.equal ByRank
-                        , Types.Route.Url.toString
+                        , Types.Route.Url.toString testConfig
                             >> Expect.equal
-                                (Route.defaultSorting
+                                (testConfig.defaultSorting
                                     == ByRank
                                     |> Utils.ifElse "/" "/?sort-by=rank"
                                 )
                         ]
             , testString "https://example.com/?sort-by=date" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal Route.NoId
                         , .parameters >> .globalFts >> nothing
                         , .parameters >> .sorting >> Expect.equal ByDate
-                        , Types.Route.Url.toString
+                        , Types.Route.Url.toString testConfig
                             >> Expect.equal
-                                (Route.defaultSorting
+                                (testConfig.defaultSorting
                                     == ByDate
                                     |> Utils.ifElse "/" "/?sort-by=date"
                                 )
                         ]
             , testString "https://example.com/123/456?search=foo" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal (Route.TwoIds (Id.fromInt 123) (Id.fromInt 456))
                         , .parameters >> .globalFts >> expectJustSearchTerm "foo"
-                        , Types.Route.Url.toString >> Expect.equal "/123/456?search=foo"
+                        , Types.Route.Url.toString testConfig >> Expect.equal "/123/456?search=foo"
                         ]
             , describe "It should remove search terms that are empty"
                 [ testString "https://example.com/?search=&offset=7" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .parameters >> .globalFts >> nothing
                             , .parameters >> .offset >> Expect.equal 7
-                            , Types.Route.Url.toString >> Expect.equal "/?offset=7"
+                            , Types.Route.Url.toString testConfig >> Expect.equal "/?offset=7"
                             ]
                 ]
             , describe "It should remove whitespace on both sides of a search term"
                 [ testString "https://example.com/789/?search=%20foo%20%20bar%20" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .path >> Expect.equal (Route.OneId (Id.fromInt 789))
                             , .parameters >> .globalFts >> expectJustSearchTerm "foo bar"
-                            , Types.Route.Url.toString >> Expect.equal "/789?search=foo%20bar"
+                            , Types.Route.Url.toString testConfig >> Expect.equal "/789?search=foo%20bar"
                             ]
                 ]
             , describe "It should remove search terms that are only whitespace"
                 [ testString "https://example.com/?search=%20%20&offset=7" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .parameters >> .globalFts >> nothing
                             , .parameters >> .offset >> Expect.equal 7
@@ -148,7 +160,7 @@ suite =
             , describe "It should remove fts filter terms that are empty or only whitespace"
                 [ testString "https://example.com/?search-title=&search-person=%20%20&search-author=foo" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .parameters >> .ftsFilters >> FilterList.get (Aspect.fromString "title") >> nothing
                             , .parameters >> .ftsFilters >> FilterList.get (Aspect.fromString "person") >> nothing
@@ -158,7 +170,7 @@ suite =
             , describe "It should keep facet filter values that are empty or contain whitespace"
                 [ testString "https://example.com/?has-title=&has-author=%20" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .parameters >> .facetFilters >> FilterList.get (Aspect.fromString "title") >> Expect.equal (Just "")
                             , .parameters >> .facetFilters >> FilterList.get (Aspect.fromString "author") >> Expect.equal (Just " ")
@@ -167,7 +179,7 @@ suite =
             , describe "Multiple search terms on the same aspect should be concatenated"
                 [ testString "https://example.com/?search=f1&search-author=a1&search=f2&search-author=a2" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .parameters >> .globalFts >> expectJustSearchTerm "f1 f2"
                             , .parameters >> .ftsFilters >> FilterList.get (Aspect.fromString "author") >> expectJustSearchTerm "a1 a2"
@@ -176,24 +188,24 @@ suite =
             , describe "On multiple values of a facet filter on the same aspect: the last value should win"
                 [ testString "https://example.com/213?has-title=t1&has-title=t2" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .parameters >> .facetFilters >> FilterList.get (Aspect.fromString "title") >> Expect.equal (Just "t2")
                             ]
                 ]
             , testString "https://example.com/789/?search-title=%20foo%20\"bar%20%20baz\"" <|
                 Url.fromString
-                    >> Maybe.andThen Types.Route.Url.parseUrl
+                    >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                     >> justAndThenAll
                         [ .path >> Expect.equal (Route.OneId (Id.fromInt 789))
                         , .parameters >> .globalFts >> nothing
                         , .parameters >> .ftsFilters >> FilterList.get (Aspect.fromString "title") >> expectJustSearchTerm "foo \"bar baz\""
-                        , Types.Route.Url.toString >> Expect.equal "/789?search-title=foo%20%22bar%20baz%22"
+                        , Types.Route.Url.toString testConfig >> Expect.equal "/789?search-title=foo%20%22bar%20baz%22"
                         ]
             , describe "Percent-coding should work also used in a path segment"
                 [ testString "https://example.com/45%36" <|
                     Url.fromString
-                        >> Maybe.andThen Types.Route.Url.parseUrl
+                        >> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         >> justAndThenAll
                             [ .path >> Expect.equal (Route.OneId (Id.fromInt 456))
                             , .parameters >> .globalFts >> nothing
@@ -205,11 +217,11 @@ suite =
                 "building and parsing again results in same route"
                 (\route ->
                     route
-                        |> Types.Route.Url.toString
+                        |> Types.Route.Url.toString testConfig
                         -- |> Debug.log "toString"
                         |> String.append "https://example.com"
                         |> Url.fromString
-                        |> Maybe.andThen Types.Route.Url.parseUrl
+                        |> Maybe.andThen (Types.Route.Url.parseUrl testConfig)
                         |> justAndThen (Expect.equal route)
                 )
             ]
