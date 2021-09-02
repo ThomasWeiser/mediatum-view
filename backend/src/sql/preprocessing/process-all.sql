7d2fa1e1bc1d7968b6d451e72f68e742d3634725
Thomas Weiser <git@thomasweiser.de>
2021-09-02 16:00:24 +0200

    Allow limiting of preprocessing by runtime parameters

--- backend/src/sql/preprocessing/process-all.sql
+++ backend/src/sql/preprocessing/process-all.sql
@@ -1,5 +1,14 @@
 
+------------------------------------
 
+-- Output possibly configured limits
+
+select
+    current_setting('mediatum.preprocessing_min_node_id', true)::int as "mediatum.preprocessing_min_node_id",
+    current_setting('mediatum.preprocessing_max_node_id', true)::int as "mediatum.preprocessing_max_node_id",
+    current_setting('mediatum.preprocessing_limit', true)::int as "mediatum.preprocessing_limit";
+
+------------------------------------
 
 truncate preprocess.ufts, preprocess.aspect;
 
@@ -12,9 +21,11 @@ set session client_min_messages to warning;
 insert into preprocess.ufts (nid, "year", recency, tsvec)
     select * 
     from preprocess.ufts_as_view
-    -- where nid > 601000 and nid < 602000 -- For testing: process some well-known documents only
-    -- where nid > 1515316
-    -- limit 2000 -- For testing the code one may just process a small fraction of the data
+    -- For testing the code one may just process a fraction of the data
+    -- by using the custom configuration parameters below
+    where (nid >= current_setting('mediatum.preprocessing_min_node_id', true)::int) is not false
+      and (nid <= current_setting('mediatum.preprocessing_max_node_id', true)::int) is not false
+    limit current_setting('mediatum.preprocessing_limit', true)::int
 ;
 
 -- Reset message level to default
@@ -31,9 +42,9 @@ set session client_min_messages to warning;
 insert into preprocess.aspect (nid, name, values, tsvec)
     select nid, name, values, tsvec
     from preprocess.aspect_view
-    -- where nid > 601000 and nid < 602000 -- For testing: process some well-known documents only
-    -- where nid > 1515316
-    -- limit 44000 -- For testing: process only a smaller number of rows
+    where (nid >= current_setting('mediatum.preprocessing_min_node_id', true)::int) is not false
+      and (nid <= current_setting('mediatum.preprocessing_max_node_id', true)::int) is not false
+    limit current_setting('mediatum.preprocessing_limit', true)::int * 10
 ;
 
 -- Reset message level to default
