6b5352df4680e1423ba5fc3c2f166f7fd818f267
Thomas Weiser <git@thomasweiser.de>
2021-09-21 15:54:55 +0200

    Configure backend depending on tier (dev vs prod)

--- backend/src/ts/main.ts
+++ backend/src/ts/main.ts
@@ -1,16 +1,27 @@
 
-
 const express = require("express");
 const { postgraphile } = require("postgraphile");
 
-const app = express();
+type Tier = 'dev' | 'prod';
+const validTiers = ['dev', 'prod'];
 
+const TIER = process.env.TIER as Tier;
 
-const databaseSuperUser = "postgres"; // Necessary for using "watch" option
+if (!TIER || !validTiers.includes(TIER)) {
+    console.error(`The TIER environment variable is required but was either not specified or is set to an invalid value: ${String(TIER)}.`);
+    process.exit(1);
+}
+
+const databaseSuperUser = "postgres"; // Necessary for using "watch" option in dev
 const port = 5000;
 const statementTimeout = "30s"
 
-const databaseConnectionUrl = "postgres://" + databaseSuperUser + "@localhost:5432/" + process.env.MEDIATUM_DATABASE_NAME;
+const databaseConnectionUser = TIER == 'dev' ? databaseSuperUser : process.env.MEDIATUM_DATABASE_USER_VIEW_API;
+const databaseConnectionUrl = "postgres://" + databaseConnectionUser + "@localhost:5432/" + process.env.MEDIATUM_DATABASE_NAME;
+
+// ----------------------------------------------------------------
+
+const app = express();
 
 app.use(
     postgraphile(
@@ -19,17 +30,17 @@ app.use(
         {
             pgDefaultRole: process.env.MEDIATUM_DATABASE_USER_VIEW_API,
             enableCors: true,
-            exportGqlSchemaPath: 'export/schema-export.graphql',
-            watchPg: true,
-            graphiql: true,
-            enhanceGraphiql: true,
+            exportGqlSchemaPath: TIER == 'dev' ? 'export/schema-export.graphql' : null,
+            watchPg: TIER == 'dev',
+            graphiql: TIER == 'dev',
+            enhanceGraphiql: TIER == 'dev',
+            allowExplain: TIER == 'dev',
             setofFunctionsContainNulls: false,
             legacyRelations: "omit",
             ignoreRBAC: false,
             pgSettings: {
                 statement_timeout: statementTimeout,
-            },
-            allowExplain: false
+            }
         }
     )
 );
